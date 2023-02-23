dcece2cb13e33fdb5b8b3c2cebef05b10db4fdf2
Tonyo Callimoutou <tonyo.callimoutou@hotmail.fr>
2023-02-23 10:54:02 +0100

    fix: db.php

--- db.php
+++ db.php
@@ -1,6 +1,32 @@
 <?php
-  $conn = new mysqli("localhost", "root", "", "animeheaven");
-  if($conn -> connect_error) {
-      die("Connection failed: " . $conn -> connect_error);
-   }
+class bd{
+
+    private $co;
+    private $host;
+    private $user;
+    private $passwd;
+    private $bdd;
+
+
+    function __construct()
+    {
+        $this->host = "localhost";
+        $this->user = "root";
+        $this->bdd = "animeheaven";
+        $this->passwd = "root";
+    }
+
+    public function getco(){
+        return $this->co;
+    }
+
+    public function connect(){
+        $this->co = mysqli_connect($this->host , $this->user , $this->passwd, $this->bdd,9906);
+    }
+
+    public function disconnect(){
+        mysqli_close($this->co);
+    } 
+
+}
 ?>
